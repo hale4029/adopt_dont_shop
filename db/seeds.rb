2bd18eacc8aabd4ce001a1efd53d22dea8da6fa3
Harrison Levin <harrison483@gmail.com>
2019-11-19 15:03:09 -0700

    rpsec passing for story 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+shelter_1 = Shelter.create(name: "Denver")
+shelter_2 = Shelter.create(name: "Littleton")
--- spec/shelters_spec.rb
+++ spec/shelters_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
+
+#story 2
+RSpec.describe "shelters index page", type: :feature do
+  it "show all shelters" do
+    shelter_1 = Shelter.create(name: "Denver")
+    shelter_2 = Shelter.create(name: "Littleton")
+
+    visit '/shelters/'
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_2.name)
+
+  end
+end
+
+#story 3
 RSpec.describe "shelters index page", type: :feature do
   it "show all shelters" do
     shelter_1 = Shelter.create(name: "Denver")
