eddd6fdb91766c12f04aaa388748843e32e36e41
Harrison Levin <harrison483@gmail.com>
2019-11-19 12:14:54 -0700

    added coverage to gitignore, edits pertaining to shetlers MVC buildout

--- spec/shelters_spec.rb
+++ spec/shelters_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "shelters index page", type: :feature do
-  it "can see all the shelters" do
-    shelter_1 = Shelter.create()
-    shelter_2 = Shelter.create()
+  it "display all shelters" do
+    shelter_1 = Shelter.create
+    shelter_2 = Shelter.create
 
     visit "/shelters"
 
     expect(page).to have_content(shelter_1)
     expect(page).to have_content(shelter_2)
-    
+
   end
 end
