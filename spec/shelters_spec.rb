31493908ea84943536efb7a771cd44e99cd886eb
Harrison Levin <harrison483@gmail.com>
2019-11-19 14:45:56 -0700

    created database and migrated name:string

--- spec/shelters_spec.rb
+++ spec/shelters_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "shelters index page", type: :feature do
-  it "display all shelters" do
+  it "show all shelters" do
     shelter_1 = Shelter.create(name: "Denver")
     shelter_2 = Shelter.create(name: "Littleton")
 
-    visit "/shelters"
+    visit '/shelters/'
 
-    expect(page).to have_content(shelter_1)
-    expect(page).to have_content(shelter_2)
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_2.name)
 
   end
 end
