cca787981c24940f2c020b201e3fb035c810cecb
Harrison Levin <harrison483@gmail.com>
2019-11-20 16:42:48 -0700

    created spec test for story 4: create new shelter

--- spec/features/shelters_spec.rb
+++ spec/features/shelters_spec.rb
@@ -21,9 +21,9 @@ describe "shelters story tests", type: :feature do
 
       expect(page).to have_content(@shelter_1.name)
       expect(page).to have_content(@shelter_2.name)
-
     end
   end
+
 
   describe "shelter id page" do
     it "show shelter (id) page and details" do
@@ -41,7 +41,41 @@ describe "shelters story tests", type: :feature do
       expect(page).to have_content(@shelter_2.address)
       expect(page).to have_content(@shelter_2.city)
       expect(page).to have_content(@shelter_2.zip)
-
     end
   end
+
+  describe "shelter create page" do
+    it "contains button and inputs" do
+
+      visit "/shelters/new"
+
+      page.should have_no_content('New Sheleter Name')
+      expect(page).to have_button("submit")
+      fill_in('shelter[name]', :with => 'Save Dog')
+      fill_in('shelter[address]', :with => '123 Pine')
+      fill_in('shelter[city]', :with => 'Denver')
+      fill_in('shelter[state]', :with => 'Colorado')
+      fill_in('shelter[zip]', :with => 80122 )
+    end
+
+    it "creates new shelter" do
+
+      visit "/shelters/new"
+
+      fill_in('shelter[name]', :with => 'New Shelter Name')
+      fill_in('shelter[address]', :with => '123 Pine')
+      fill_in('shelter[city]', :with => 'Denver')
+      fill_in('shelter[state]', :with => 'Colorado')
+      fill_in('shelter[zip]', :with => 80122 )
+      find_button('submit').click
+
+      expect(page).to have_content("New Shelter Name")
+
+
+
+    end
+
+
+  end
+
 end
